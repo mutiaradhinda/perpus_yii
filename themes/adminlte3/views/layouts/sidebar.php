a363e858fdf1c071157f35fd6771f2322abda7c2
mzaedan <zaedanm108@gmail.com>
2022-12-28 09:04:01 +0700

    Update

--- themes/adminlte3/views/layouts/sidebar.php
+++ themes/adminlte3/views/layouts/sidebar.php
@@ -19,7 +19,7 @@ use hail812\adminlte3\widgets\Menu;
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('lte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= Yii::getAlias('@web'); ?>/images/no-photo.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Admin</a>
